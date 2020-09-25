8d6ac4e8415665ab8e0ba641fb3e3beaacff9b8e
Nosler <andinosler@gmail.com>
2020-09-25 14:55:58 -0400

    Fix typos

--- racbot.js
+++ racbot.js
@@ -36,8 +36,8 @@ var facts = [
    'The comic book character Rocket Raccoon was directly inspired by Paul McCartney’s faux-western song, Rocky Raccoon.',
    'Raccoons communicate with each other using over 200 different sounds and 12-15 different calls.',
    'Raccoons are capable of opening doors, jars, bottles and latches.',
-   'There has only been one recorded human death from raccoon rabies in the United States.',
-   'Raccoons can withstand a drops of 35 feet.',
+   'As of 2020, there has only been one recorded human death from raccoon rabies in the United States.',
+   'A raccoon can withstand a drop of 35 feet.',
    'On average, Adult raccoons weigh 15 to 40 pounds. However, there have been specimens from urban areas that weigh up to 60 pounds.',
    'Raccoons make several types of noises, including purrs, chittering sounds, and various growls, snarls, and snort.',
    'Raccoons can rotate their hind feet 180 degrees and descend trees headfirst.',
@@ -49,7 +49,6 @@ var facts = [
    'The Spanish word for raccoons, *mapache* was borrowed from the Aztec Nahualti word *mapachitli*, meaning \'[the] one who takes everything in its hands\'.',
    'The famous Sioux leader Spotted Tail took his name afrom a raccoon skin hat he acquired from a fur trader.',
    'When fed cat food over a long period of time, raccoons can develop gout.',
-   'As of 1987, the raccoon was identified as the most important wild furbearer in terms of revenue.',
    'The Dakota Sioux believe the raccoon has natural spirit powers, since its mask resembles the facial paintings (two-fingered swashes of black and white) used during rituals to connect to spirit beings.',
    'The percentage of urban raccoons sleeping in abandoned or occupied houses varies from 15-43%. With the lowest being Washginton DC, USA and the highest being Kassel, Germany.'
 ];
